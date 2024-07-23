e9edc59b1fb4b7d7fd3ca3f9bba1025c9dbf6b68
Ramrbs1 <86314526+Ramrbs1@users.noreply.github.com>
2024-07-23 06:22:21 +0000

    correct day6 code

--- day6.py
+++ day6.py
@@ -1,16 +1,16 @@
 email_id = input("Enter your email id: ")
 last_Char = email_id[-4:]
 #print(last_Char)
-found = False
+found = 0
 
 for i in email_id:
   if i == "@":
-    found = True
+    found += 1
     #print("Symbol @ found in the given email id so Moving ahead")
     #break
 
 #print(found)
-if found == True and last_Char == ".com":
+if found == 1 and last_Char == ".com":
   print(email_id, "This is valid Email id")
 else:
   print(email_id, "This is invalid Email id")
